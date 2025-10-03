8bb6a85909514844823b65d72068db156f92034f
Subodh <subodhlst@gmail.com>
2025-10-03 17:07:07 +0530

    handle hover event

--- src/StarRating.js
+++ src/StarRating.js
@@ -18,6 +18,7 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   const [rating, setRating] = useState(0);
+  const [tempRating, setTempRating] = useState(0);
 
   function handleRating(rating) {
     setRating(rating);
@@ -30,10 +31,12 @@ export default function StarRating({ maxRating = 5 }) {
           <Star
             onRate={() => handleRating(i + 1)}
             key={i}
-            onFull={rating >= i + 1}
+            onFull={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+            onHoverIn={() => setTempRating(i + 1)}
+            onHoverOut={() => setTempRating(0)}
           />
         ))}
-        <p style={textStyle}>{rating || ""}</p>
+        <p style={textStyle}>{tempRating || rating || ""}</p>
       </div>
     </div>
   );
@@ -46,9 +49,15 @@ const starStyle = {
   display: " block",
 };
 
-function Star({ onRate, onFull }) {
+function Star({ onRate, onFull, onHoverIn, onHoverOut }) {
   return (
-    <span role="button" style={starStyle} onClick={onRate}>
+    <span
+      role="button"
+      style={starStyle}
+      onClick={onRate}
+      onMouseEnter={onHoverIn}
+      onMouseLeave={onHoverOut}
+    >
       {onFull ? (
         <svg
           xmlns="http://www.w3.org/2000/svg"
