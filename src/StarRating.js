c52626f3721213a21c311deeb41f15fe889eac3f
Subodh <subodhlst@gmail.com>
2025-10-03 15:48:10 +0530

    build resuable star rating componnent

---
+++ src/StarRating.js
@@ -0,0 +1,29 @@
+const containerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "16px",
+};
+
+const starContainerStyle = {
+  display: "flex",
+  alignItems: "center",
+  gap: "4px",
+};
+
+const textStyle = {
+  lineHeight: "1",
+  margin: "0",
+};
+
+export default function StarRating({ maxRating = 5 }) {
+  return (
+    <div style={containerStyle}>
+      <div style={starContainerStyle}>
+        {Array.from({ length: maxRating }, (_, i) => (
+          <span>S{i + 1}</span>
+        ))}
+        <p style={textStyle}>10</p>
+      </div>
+    </div>
+  );
+}
